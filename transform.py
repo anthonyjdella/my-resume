d3ce8f494e0f2e906e3e27d125c93c8345242270
Anthony_Dellavecchia <leeznon@gmail.com>
2021-10-08 00:23:41 -0500

    feat(transform): transform education and work items

--- transform.py
+++ transform.py
@@ -15,4 +15,29 @@ with open('resume-pre-transform.json') as json_data:
     for language in data['languages'][:]:
         data['languages'].remove(language)
 
+    for institution in data['education'][:]:
+        if (institution['institution'] == 'The University of Texas at Austin'):
+            data['education'].remove(institution)
+        print(institution.pop('courses'))
+        # for course in data['courses'][:]:
+        #     print(course)
+
+    for item in data['volunteer'][:]:
+        data['volunteer'].remove(item)
+
+    for job in reversed(data['work'][:]):
+        if (job['name'] not in {'State Farm ®', 
+                                    'Bank of America',
+                                    'GM Financial'}):
+            data['work'].remove(job)
+
+    startDate = ''
+
+    for i, job in enumerate(reversed(data['work'][:])):
+        if (i % 2 == 0):
+            startDate = job['startDate']
+            data['work'].remove(job)
+        else:
+            job['startDate'] = startDate
+
     print(json.dumps(data, indent=4))
